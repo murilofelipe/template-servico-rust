103ef2091e1232f449210acf79194a37bac655ba
Murilo Felipe <murilosilva.felipe@gmail.com>
2026-08-12 07:31:09 -0400

    chore: setup initial rust project structure

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Serviço Rust Template rodando!" }));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("Server running on http://0.0.0.0:3000");
+    axum::serve(listener, app).await.unwrap();
+}
